5bc8a9eb0b5dddce5bd74d03efc880ed22d52c47
Marllon Ramos <marllonsteam@gmail.com>
2024-11-19 00:21:23 -0800

    marllonramoss (Alteracao do card-title)

--- src/components/Devs/CardMarllon.jsx
+++ src/components/Devs/CardMarllon.jsx
@@ -1,7 +1,7 @@
 export default function CardMarllon() {
     return ( 
         <div class="card">
-            <h3 class="card-title">Card Title</h3>
+            <h3 class="card-title">Marllon Ramos</h3>
             <p class="card-content">This is some basic content inside the card. You can add more details here.</p>
             <button class="card-button">Learn More</button>
         </div>
