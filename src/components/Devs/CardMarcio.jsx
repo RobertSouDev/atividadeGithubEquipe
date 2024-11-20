22e261b389593481f5636abb9d13578ec7b3b715
fulllagado1student1 <fulllagadostudent@gmail.com>
2024-11-20 21:34:56 +0000

    Marcio Adicionados Nome e Descrição

--- src/components/Devs/CardMarcio.jsx
+++ src/components/Devs/CardMarcio.jsx
@@ -1,8 +1,8 @@
 export default function CardMarcio() {
     return ( 
         <div class="card">
-            <h3 class="card-title">Card Title</h3>
-            <p class="card-content">This is some basic content inside the card. You can add more details here.</p>
+            <h3 class="card-title">Marcio Guilherme</h3>
+            <p class="card-content">Eterno Aprendiz</p>
             <button class="card-button">Learn More</button>
         </div>
      );
