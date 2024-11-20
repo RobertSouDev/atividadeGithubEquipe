74ee74896f3ac205fcfe4d740e614a7c0b6c8501
fernandes <storeshopad@gmail.com>
2024-11-20 18:48:58 -0300

    Rafael Fernandes (Alteração no título e conteúdo e Label do Botão)

--- src/components/Devs/CardRafael.jsx
+++ src/components/Devs/CardRafael.jsx
@@ -1,9 +1,9 @@
 export default function CardRafael() {
     return ( 
         <div class="card">
-            <h3 class="card-title">Card Title</h3>
-            <p class="card-content">This is some basic content inside the card. You can add more details here.</p>
-            <button class="card-button">Learn More</button>
+            <h3 class="card-title">Rafael Fernandes</h3>
+            <p class="card-content">Rafael por aqui.</p>
+            <button class="card-button">Formação Dev</button>
         </div>
      );
 }
