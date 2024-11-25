d9b45880a2bae41546ab52474533840ee05c0de9
FlavioDemetrio <flademetrio@gmail.com>
2024-11-25 13:22:42 -0300

    Flávio (alterado CardFlavio.jsx)

--- src/components/Ouvinte/CardFlavio.jsx
+++ src/components/Ouvinte/CardFlavio.jsx
@@ -1,9 +1,9 @@
 export default function CardFlavio() {
     return ( 
         <div class="card">
-            <h3 class="card-title">Card Title</h3>
-            <p class="card-content">This is some basic content inside the card. You can add more details here.</p>
-            <button class="card-button">Learn More</button>
+            <h3 class="card-title">Flávio Demétrio</h3>
+            <p class="card-content">Vou participar como ouvinte.</p>
+            <button class="card-button">Flávio</button>
         </div>
      );
 }
